9810fa668eedfadcb5dfc846e85f1303d7ab0f4f
Alex Titov <54718346+alextitov19@users.noreply.github.com>
2024-09-27 09:49:27 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 docker pull ucmercedandeslab/tinyos_debian
-docker run -v $(pwd):/home/cse160 -ti ucmercedandeslab/tinyos_debian
+docker run --name cse160 -v $(pwd):/home/cse160 -ti ucmercedandeslab/tinyos_debian
